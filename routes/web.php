1ad235e050b4cc76c92df70bd16c9dd0a3c2bb2f
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-10 19:34:37 -0300

    Correct conflict leftovers

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
 Route::get('/edit-user', function (){
   return view('edit-user');
 });
@@ -23,8 +22,8 @@ Route::get('/add-project', 'AddProjectController@showForm');
 
 //Post de add-project
 Route::post('/add-project', 'AddProjectController@addProject');
-=======
+
 Route::get('/edit-user', 'User_edit_controller@displayView')->name('edit-user');
 		
 Route::post('/edit-user', 'User_edit_controller@updateUserInfo')->name('edit-user');
->>>>>>> boolLaravel
+
