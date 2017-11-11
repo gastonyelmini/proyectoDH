c88061b4a28dcf84cb7ecd6030c85a1fd405a450
Gaston Yelmini <gastonyelmini@gmail.com>
2017-11-11 19:35:31 -0300

    Add routes autentication to avoid not logged in users

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,25 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+/*
+Rutas realacionadas con editar usuario
+Rutas Juan
+*/
+Route::post('/edit-user', 'User_edit_controller@updateUserInfo')->name('edit-user');
+Route::get('/edit-user', 'User_edit_controller@displayView')->name('edit-user');
 Route::post('/edit-user', 'User_edit_controller@updateUserInfo')->name('edit-user');
 
 /*
 Rutas relacionadas con proyectos
+Rutas Gaston
+Con 'middleware' => 'auth' evito que el usuario pueda ingresar a
+las paginas sin estar logueado
 */
-Route::get('/add-project', 'AddProjectController@showForm')->name('add-project');
-
-//Post de add-project
-Route::post('/add-project', 'AddProjectController@addProject')->name('add-project');
-
-Route::get('/edit-user', 'User_edit_controller@displayView')->name('edit-user');
-		
-Route::post('/edit-user', 'User_edit_controller@updateUserInfo')->name('edit-user');
-
+Route::get('/add-project', [
+    'middleware' => 'auth',
+    'uses' => 'AddProjectController@showForm'
+])->name('add-project');
+Route::post('/add-project', [
+    'middleware' => 'auth',
+    'uses' => 'AddProjectController@addProject'
+])->name('add-project');
