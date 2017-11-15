36295069e45f8be297ffdf6b11361a65a2758d7e
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-15 15:39:59 -0300

    Correct add-task file indentation

--- resources/views/project/add-task.blade.php
+++ resources/views/project/add-task.blade.php
@@ -11,45 +11,46 @@
 </div>
 @endif
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Add a new task</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="/add-project">
-                        {{ csrf_field() }}
-                        
-                        <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                            <label for="title" class="col-md-4 control-label">Name</label>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">Add a new task</div>
+        <div class="panel-body">
+          <form class="form-horizontal" method="POST" action="/add-project">
+          	{{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="title" type="text" class="form-control" name="title" placeholder="Plase, insert a task name" value="{{ old('title') }}" required autofocus>
+          	<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+              <label for="title" class="col-md-4 control-label">Name</label>
 
-                                @if ($errors->has('title'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('title') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="date" class="col-md-4 control-label">Date</label>
-                            <div class="col-md-6 ">
+              <div class="col-md-6">
+                <input id="title" type="text" class="form-control" name="title" placeholder="Plase, insert a task name" value="{{ old('title') }}" required autofocus>
 
-														</div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Submit
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+								@if($errors->has('title'))
+								  <span class="help-block">
+								    <strong>{{ $errors->first('title') }}</strong>
+								  </span>
+								@endif
+
+              </div>
+          	</div>
+            <div class="form-group">
+              <label for="date" class="col-md-4 control-label">Date</label>
+              <div class="col-md-6 ">
+
+							</div>
             </div>
-        </div>
+            <div class="form-group">
+              <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </div>
+        	</form>
+      	</div>
+      </div>
     </div>
+  </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
