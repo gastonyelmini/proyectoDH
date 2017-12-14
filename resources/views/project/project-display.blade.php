f40647c62b0590997cf386f4fa9258250989fe41
Gaston Yelmini <gastonyelmini@gmail.com>
2017-12-14 00:25:29 -0300

    Add id to specific project to retrieve it from js controller

--- resources/views/project/project-display.blade.php
+++ resources/views/project/project-display.blade.php
@@ -13,9 +13,9 @@
     <!-- /.col-lg-12 -->
 </div>
 <div class="row">
-    <div class="col-sm-12">
+    <div class="col-sm-12" id="{{ $projectRequested->id }}">
         <div class="white-box">
-            <h3 class="box-title m-b-0">Project {{ $projectRequested->title }}</h3>
+            <h3 class="box-title m-b-0">Project <strong>{{ $projectRequested->title }}</strong></h3>
             <p class="text-muted m-b-30 font-13"> This is the actual project</p>
             <div class="gantt-container">
 			    <svg id="gantt" width="400" height="600"></svg>
