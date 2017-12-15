cfb463dc8659f2202efd8ca66d56201a4c952ae0
Gaston Yelmini <gastonyelmini@gmail.com>
2017-12-15 08:23:58 -0300

    Delete some extra template components

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
         $jsonWeather = file_get_contents("http://apidev.accuweather.com/currentconditions/v1/7894.json?language=en&apikey=hoArfRosT1215");
         $weather = json_decode($jsonWeather, true);
         //Get number of active projects
-        $activeProjects = DB::table('projects')->where("author_id", auth()->user()->id)->count();
+        $activeProjects = DB::table('projects')->where("author_id", auth()->user()->id)->get();
         $tasksAsigned = count(DB::table('tasks_users')->where('id_user',auth()->user()->id)->get());
         $actualUser = Auth::User();
         $projects_user = count(DB::table('projects_users')->where("id_user", auth()->user()->id)->get());
