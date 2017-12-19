6256ab905689cd60df6a459b34986247e37bdccb
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-19 16:12:13 -0300

    Correct gant date movement

--- public/js/frappe-gantt/controller/gant_controller.js
+++ public/js/frappe-gantt/controller/gant_controller.js
@@ -1,63 +1,63 @@
-if (document.querySelector(".gantt-master")) {
+if (document.querySelector('.gantt-master')) {
   /* Busco el id de proyecto que le asigne en el blade */
-  var projectId = document.querySelector(".gantt-master").id;
-  var tasksArray = [];
-  var gantTasks = [];
+  var projectId = document.querySelector('.gantt-master').id
+  var tasksArray = []
+  var gantTasks = []
 
   axios
-    .get("/get-project-axios/" + projectId)
+    .get('/get-project-axios/' + projectId)
     .then(function(response) {
       if (!response.data.error) {
-        tasksArray = Object.entries(response.data.projectTasks);
+        tasksArray = Object.entries(response.data.projectTasks)
 
         for (var i = 0; i < tasksArray.length; i++) {
-          var singleTask = Object.entries(tasksArray[i][1]);
+          var singleTask = Object.entries(tasksArray[i][1])
 
           gantTasks.push({
             id: singleTask[0][1],
             name: singleTask[1][1],
             start: singleTask[2][1],
             end: singleTask[3][1],
-            progress: singleTask[9][1]
-          });
+            progress: singleTask[9][1],
+          })
         }
 
-        var gantt_chart = Gantt("#gantt", gantTasks, {
+        var gantt_chart = Gantt('#gantt', gantTasks, {
           on_click: function(task) {
-            console.log(task);
+            console.log(task)
           },
           on_date_change: function(task, start, end) {
-            console.log(start);
-            axios
-              .get(
-                "/update-task-time/" +
-                  task["id"] +
-                  "/" +
-                  start["_i"] +
-                  "/" +
-                  end["_i"]
-              )
-              .then(function(response) {})
-              .catch(function(error) {
-                alert(error);
-              });
+            console.log(start)
+            // axios
+            //   .get(
+            //     "/update-task-time/" +
+            //       task["id"] +
+            //       "/" +
+            //       start["_i"] +
+            //       "/" +
+            //       end["_i"]
+            //   )
+            //   .then(function(response) {})
+            //   .catch(function(error) {
+            //     alert(error);
+            //   });
           },
           on_progress_change: function(task, progress) {
             axios
-              .get("/update-task-progress/" + task["id"] + "/" + progress)
+              .get('/update-task-progress/' + task['id'] + '/' + progress)
               .then(function(response) {})
               .catch(function(error) {
-                alert(error);
-              });
+                alert(error)
+              })
           },
           on_view_change: function(mode) {
-            console.log(mode);
-          }
-        });
-        console.log(gantt_chart);
+            console.log(mode)
+          },
+        })
+        console.log(gantt_chart)
       }
     })
     .catch(function(error) {
-      alert(error);
-    });
+      alert(error)
+    })
 }
